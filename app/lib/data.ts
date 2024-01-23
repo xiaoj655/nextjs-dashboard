08cd9e5e149ae4df866813c0149fcd9329b1e161
brook <123456@gmai.com>
2024-01-23 22:55:44 +0800

    a

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,10 +9,12 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
