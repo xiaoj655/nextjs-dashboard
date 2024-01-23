e37c323a463dc99ef0c4f102dc30f45f8852b94c
brook <123456@gmai.com>
2024-01-23 23:14:51 +0800

    23

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,9 +3,11 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { Suspense } from 'react'
+import { RevenueChartSkeleton } from '../ui/skeletons';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
+  // const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices,
         numberOfCustomers} = await fetchCardData();
@@ -25,7 +27,10 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+        {/* <RevenueChart revenue={revenue}  /> */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
