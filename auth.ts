0b6fc5d2351f7d2c52637aa515aa2f28f3f446ed
brook <123456@gmai.com>
2024-01-26 13:09:56 +0800

    i123

---
+++ auth.ts
@@ -0,0 +1,43 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+
+async function getUser(email: string): Promise<User | null> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email = ${email}`;
+    return user.rows[0];
+  } catch (err) {
+    console.log(err);
+    throw new Error('Error getting user');
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (passwordMatch) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+});
